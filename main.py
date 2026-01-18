d24d165217c5d04b563604f3c53a907667952d73
gailcliff <gailcliff100@gmail.com>
2026-01-17 21:47:04 -0500

    simple game play with elo updates

--- main.py
+++ main.py
@@ -1,22 +1,66 @@
 import random
+from dataclasses import dataclass
 
+@dataclass
 class Player:
     id: str
     rating: int
 
+    def __str__(self):
+        return f"{self.id} (elo={self.rating})"
+
 win = 1
 draw = 0.5
 loss = 0
+K = 32
 
 ratings: dict[str, int] = {}
 
+def get_player_expectations(challenger: Player, defender: Player) -> tuple[float, float]:
+    expectation_challenger = 1 / (1 + 10**((defender.rating - challenger.rating) / 400))
+    expectation_defender = 1 - expectation_challenger
 
-def play_game(player1: Player, player2: Player) -> int:
+    return (expectation_challenger, expectation_defender)
 
-    return random.choice([win, draw, loss])
 
-def update_elo_scores(player1: Player, player2: Player, result: int) -> None:
-    pass
+def update_elo_scores(challenger: Player, defender: Player, challenger_new_rating: int, defender_new_rating: int) -> None:
+    ratings[challenger.id] = challenger_new_rating
+    ratings[defender.id] = defender_new_rating
+    challenger.rating = challenger_new_rating
+    defender.rating = defender_new_rating
+
+
+def play_game(challenger: Player, defender: Player) -> int:
+
+    expectation_challenger, expectation_defender = get_player_expectations(challenger, defender)
+
+    game_result = random.choice([win, draw, loss])
+
+    challenger_new_rating = round(challenger.rating + K * (game_result - expectation_challenger))
+    defender_new_rating = round(defender.rating + K * ((1 - game_result) - expectation_defender))
+
+    update_elo_scores(challenger, defender, challenger_new_rating, defender_new_rating)
+
+    return game_result
+
+
+def create_player(username: str, rating=1200) -> Player:
+    ratings[username] = rating
+
+    return Player(username, rating)
+
+
+def get_rating(player: Player):
+    return ratings[player.id]
+
 
 if __name__ == '__main__':
-    pass
+    playerA = create_player("hikaru")
+    playerB = create_player("magnus", 1300)
+
+    game_result = play_game(playerA, playerB)
+    print(f"Game: {playerA} vs. {playerB}")
+    print(f"Game result: {playerA.id}: {['loss', 'draw', 'wins'][int(game_result * 2)]}")
+
+    print(f"{playerA.id} new ELO: {get_rating(playerA)}")
+    print(f"{playerB.id} new ELO: {get_rating(playerB)}")
