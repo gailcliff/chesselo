d78209e022e81d5ab4214e3618ec2463fade072f
gailcliff <gailcliff100@gmail.com>
2026-01-17 20:41:12 -0500

    initial setup

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from chesselo!")
+import random
+
+class Player:
+    id: str
+    rating: int
+
+win = 1
+draw = 0.5
+loss = 0
+
+ratings: dict[str, int] = {}
 
 
-if __name__ == "__main__":
-    main()
+def play_game(player1: Player, player2: Player) -> int:
+
+    return random.choice([win, draw, loss])
+
+def update_elo_scores(player1: Player, player2: Player, result: int) -> None:
+    pass
+
+if __name__ == '__main__':
+    pass
